0278bc59d953ba4eefe8a3f709881db4fefaff60
Daniil <vetusst@hotmail.com>
2021-12-06 11:00:02 +0100

    solutions for day 5

---
+++ day5/solution1.js
@@ -0,0 +1,40 @@
+// Day 5
+
+// Solution 1
+
+const fs = require("fs");
+let input = fs.readFileSync('./day5/input.txt', {
+    encoding: 'utf-8'
+})
+
+input = input.split('\n')
+
+const solution1 = (input) => {
+    let coveredPoints = []
+
+    for (let i = 0; i < input.length; i++) {
+        input[i] = input[i].split(/[^\d]+/).filter(x => /\d/.test(x)).map(x => Number(x))
+
+        let point = input[i]
+        if (point[0] === point[2]) {
+            for (let j = Math.min(point[1], point[3]); j < Math.max(point[1], point[3]) + 1; j++) coveredPoints.push([point[0], j])
+        } else if (point[1] === point[3]) {
+            for (let j = Math.min(point[0], point[2]); j < Math.max(point[0], point[2]) + 1; j++) coveredPoints.push([j, point[1]])
+        }
+    }
+
+    let counter = 0
+    for (let i = 0; i < coveredPoints.length; i++) {
+
+        let initLength = coveredPoints.length
+        for (let j = i + 1; j < coveredPoints.length; j++) {
+            if (coveredPoints[i][0] === coveredPoints[j][0] && coveredPoints[i][1] === coveredPoints[j][1]) coveredPoints.splice(j, 1)
+        }
+
+        if (initLength > coveredPoints.length) counter++
+    }
+    
+    return counter
+}
+
+console.log(solution1(input))
